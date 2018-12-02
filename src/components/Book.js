f26fcc5619849b041cf0a7c8c71e32afc756204b
Abdeljalil0Cyclo <abdeljalil.aitetaleb@gmail.com>
2018-12-02 21:12:12 +0100

    booking sections

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,18 +5,39 @@ export default class Book extends Component {
     return (
       <Fragment>
         <section className="section-book">
-                <div className="row">
-                   <div className="book">
-                   <form action="#" id="name" placeholder="full name" className="form">
-                     <div className="form__group">
-                     </div>
-                     <div className="form__group">
-                     </div>
-                     <div className="form__group">
-                     </div>
-                   </form>
-                   </div>
-                </div>
+        <div className="row">
+          <div className="book">
+          <form action="#" className="book__form">
+           <div className="u-center-text u-margin-bottom-meduim">
+                <h2 className="heading-secondary">
+                 start Booking now
+                </h2>
+           </div>
+            <div className="form__group">
+              <input type="text" id="name" placeholder="Full name" className="form__input"/>
+              <label htmlFor="name" className="form__label">Full Name</label>
+            </div>
+            <div className="form__group">
+              <input type="email" id="email" placeholder="Email adresse" className="form__input"/>
+              <label htmlFor="email" className="form__label">Email Adresse</label>
+            </div>
+            <div className="form__group">
+             <div className="form__radio-group">
+              <input type="radio" id="small" className="form__radio-input" name="size"/>
+              <label htmlFor="small" className="form__radio-label">
+              <span className="form__radio-button"></span>
+              Small tour group</label>
+             </div>
+             <div className="form__radio-group">
+              <input type="radio" id="large" className="form__radio-input" name="size"/>
+              <label htmlFor="large" className="form__radio-label">
+              <span className="form__radio-button"></span>
+              large tour group</label>
+             </div>
+            </div>
+          </form>
+          </div>
+        </div>
         </section>
       </Fragment>
     )
